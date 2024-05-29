561dce47eb5ad6b1c5cdd6f840a622c48cabebab
carlosherrerah <carlos.herrera@upa.edu.mx>
2024-05-29 11:47:19 -0600

    check Clientes

--- U01/p02/src/main/resources/Tabla.sql
+++ U01/p02/src/main/resources/Tabla.sql
@@ -1,11 +1,14 @@
 CREATE TABLE clientes (
-    id_cliente INT auto_increment PRIMARY KEY,
+    id_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(30),
     apellido VARCHAR(30),
     nacimiento date,
-    genero VARCHAR(1) CHECK (genero IN ('H', 'M', 'X')),
-    edo_civil int check (edo_civil between 0 and 2), 
+    genero VARCHAR(1),
+    edo_civil int,
     estatus boolean,
-    creado_en date
+    creado_en date, 
+    CONSTRAINT CHK_genero CHECK (genero IN ('H', 'M', 'X')),
+    CONSTRAINT CHK_EdoCivil CHECK (edo_civil between 0 and 2)
 );
+;
 
